e517ea3f1a7df2ae0d6a0d22a907ecf2536f73c6
Bob Ran <bob.ran@tno.nl>
2014-05-19 16:07:43 +0200

    uncontrolled configurable simulation added (oposite of pv panel simulation)

---
+++ flexiblepower.uncontrolled.configurable.simulation/src/org/flexiblepower/uncontrolled/configurable/simulation/Weather.java
@@ -0,0 +1,26 @@
+package org.flexiblepower.uncontrolled.configurable.simulation;
+
+public enum Weather {
+
+    moon {
+        @Override
+        double getProduction(double randomFactor, double cloudy, double sunny) {
+            return 0;
+        }
+    },
+    clouds {
+        @Override
+        double getProduction(double randomFactor, double cloudy, double sunny) {
+            return cloudy + cloudy * randomFactor;
+        }
+    },
+    sun {
+        @Override
+        double getProduction(double randomFactor, double cloudy, double sunny) {
+            return sunny + (sunny / 10) * randomFactor;
+        }
+    };
+
+    abstract double getProduction(double randomFactor, double cloudy, double sunny);
+
+}
