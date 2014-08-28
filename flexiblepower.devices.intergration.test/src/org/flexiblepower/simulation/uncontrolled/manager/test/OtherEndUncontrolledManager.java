88ff389f97e9b7d9122bb38082311a5e1b0dcca3
broeninkeg <gerben.broenink@tno.nl>
2014-08-28 09:16:18 +0200

    first implementation of test uncontrolled manager integration test
    not finished yet!

--- flexiblepower.devices.intergration.test/src/org/flexiblepower/simulation/uncontrolled/manager/test/OtherEndUncontrolledManager.java
+++ flexiblepower.devices.intergration.test/src/org/flexiblepower/simulation/uncontrolled/manager/test/OtherEndUncontrolledManager.java
@@ -1,15 +1,55 @@
 package org.flexiblepower.simulation.uncontrolled.manager.test;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import junit.framework.Assert;
+
+import org.flexiblepower.efi.uncontrolled.UncontrolledAllocation;
+import org.flexiblepower.efi.uncontrolled.UncontrolledRegistration;
+import org.flexiblepower.efi.uncontrolled.UncontrolledUpdate;
+import org.flexiblepower.messaging.Cardinality;
 import org.flexiblepower.messaging.Connection;
 import org.flexiblepower.messaging.Endpoint;
 import org.flexiblepower.messaging.MessageHandler;
+import org.flexiblepower.messaging.Port;
+import org.flexiblepower.rai.comm.AllocationRevoke;
+import org.flexiblepower.rai.comm.AllocationStatusUpdate;
+import org.flexiblepower.rai.comm.ControlSpaceRevoke;
+import org.flexiblepower.rai.comm.ResourceMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Port(name = "uncontrolled",
+      accepts = { UncontrolledRegistration.class,
+                 UncontrolledUpdate.class,
+                 AllocationStatusUpdate.class,
+                 ControlSpaceRevoke.class },
+      sends = { UncontrolledAllocation.class, AllocationRevoke.class },
+      cardinality = Cardinality.SINGLE)
 public class OtherEndUncontrolledManager implements Endpoint {
+    private static final Logger log = LoggerFactory.getLogger(OtherEndUncontrolledManager.class);
+    private volatile Connection connection;
+    private final BlockingQueue<ResourceMessage> messages = new LinkedBlockingQueue<ResourceMessage>();
 
     @Override
     public MessageHandler onConnect(Connection connection) {
-        // TODO Auto-generated method stub
-        return null;
+        this.connection = connection;
+        return new MessageHandler() {
+            @Override
+            public void handleMessage(Object message) {
+                log.info("message received");
+                Assert.assertTrue(ResourceMessage.class.isAssignableFrom(message.getClass()));
+                ResourceMessage resourceMessage = (ResourceMessage) message;
+                log.debug("Received uncontrolled resourceMessage");
+                messages.add(resourceMessage);
+            }
+
+            @Override
+            public void disconnected() {
+                OtherEndUncontrolledManager.this.connection = null;
+            }
+        };
     }
 
 }
--- flexiblepower.devices.intergration.test/src/org/flexiblepower/simulation/uncontrolled/manager/test/UncontrolledManagerSimulationTest.java
+++ flexiblepower.devices.intergration.test/src/org/flexiblepower/simulation/uncontrolled/manager/test/UncontrolledManagerSimulationTest.java
@@ -1,31 +1,45 @@
 package org.flexiblepower.simulation.uncontrolled.manager.test;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import org.flexiblepower.efi.uncontrolled.UncontrolledUpdate;
 import org.flexiblepower.messaging.Endpoint;
 import org.flexiblepower.messaging.Port;
+import org.flexiblepower.simulation.pvpanel.PVSimulation;
+import org.flexiblepower.simulation.pvpanel.test.OtherEndPVPanel;
 import org.flexiblepower.simulation.test.SimulationTest;
+import org.flexiblepower.uncontrolled.manager.UncontrolledManager;
+import org.osgi.service.cm.Configuration;
 import org.osgi.util.tracker.ServiceTracker;
 
 @Port(name = "manager", accepts = UncontrolledUpdate.class)
 public class UncontrolledManagerSimulationTest extends SimulationTest {
-    private ServiceTracker<Endpoint, Endpoint> uncontrolledManagerSimulationTracker;
+    private ServiceTracker<Endpoint, Endpoint> uncontrolledManagerTracker;
+    private ServiceTracker<Endpoint, Endpoint> pvpanelSimulationTracker;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
-        uncontrolledManagerSimulationTracker = new ServiceTracker<Endpoint, Endpoint>(bundleContext,
-                                                                                      bundleContext.createFilter("(test=pvsim)"),
-                                                                                      null);
-        uncontrolledManagerSimulationTracker.open();
+        pvpanelSimulationTracker = new ServiceTracker<Endpoint, Endpoint>(bundleContext,
+                                                                          bundleContext.createFilter("(test=pvsim)"),
+                                                                          null);
+        pvpanelSimulationTracker.open();
+
+        uncontrolledManagerTracker = new ServiceTracker<Endpoint, Endpoint>(bundleContext,
+                                                                            bundleContext.createFilter("(test=uncontrolledManager)"),
+                                                                            null);
+        uncontrolledManagerTracker.open();
     }
 
     @Override
     protected void tearDown() throws Exception {
         destroy();
-        uncontrolledManagerSimulationTracker.close();
+        uncontrolledManagerTracker.close();
+        pvpanelSimulationTracker.close();
         super.tearDown();
     }
 
@@ -40,6 +54,60 @@ public class UncontrolledManagerSimulationTest extends SimulationTest {
         // config.delete();
         // config = null;
         // }
+    }
+
+    private volatile Configuration pvPanelConfig;
+    private volatile Configuration uncontrolledManagerConfig;
+    private PVSimulation pvSimulation = null;
+    private UncontrolledManager uncontrolledManager = null;
+
+    private OtherEndPVPanel create(int updateFrequency,
+                                   double powerWhenStandBy,
+                                   double powerWhenCloudy,
+                                   double powerWhenSunny) throws Exception {
+        pvPanelConfig = configAdmin.createFactoryConfiguration("org.flexiblepower.simulation.pvpanel.PVSimulation",
+                                                               null);
+        Dictionary<String, Object> pvPanelProperties = new Hashtable<String, Object>();
+        pvPanelProperties.put("updateFrequency", updateFrequency);
+        pvPanelProperties.put("powerWhenStandBy", powerWhenStandBy);
+        pvPanelProperties.put("powerWhenCloudy", powerWhenCloudy);
+        pvPanelProperties.put("powerWhenSunny", powerWhenSunny);
+        pvPanelProperties.put("test", "pvsim");
+        pvPanelConfig.update(pvPanelProperties);
+
+        pvSimulation = (PVSimulation) pvpanelSimulationTracker.waitForService(1000);
+
+        assertNotNull(pvSimulation);
+
+        uncontrolledManagerConfig = configAdmin.createFactoryConfiguration("org.flexiblepower.simulation.pvpanel.PVSimulation",
+                                                                           null);
+        Dictionary<String, Object> uncontrolledManagerProperties = new Hashtable<String, Object>();
+        uncontrolledManagerProperties.put("test", "uncontrolledManager");
+        uncontrolledManagerConfig.update(uncontrolledManagerProperties);
+
+        uncontrolledManager = (UncontrolledManager) uncontrolledManagerTracker.waitForService(1000);
+
+        assertNotNull(uncontrolledManager);
+
+        // OtherEndPVPanel otherEnd = new OtherEndPVPanel();
+        // otherEndRegistration = bundleContext.registerService(Endpoint.class, otherEnd, null);
+
+        connectionManager.autoConnect();
+
+        simulation.startSimulation(new Date(), 50);
+
+        // PowerState initialState = otherEnd.getState();
+        // assertEquals(selfDischargePower, initialState.getSelfDischargeSpeed().doubleValue(SI.WATT), 0.01);
+        // TODO add assertions.... (What can we assert here?)
+
+        // return otherEnd;
+        return null;
+    }
+
+    public void testCreate() throws Exception {
+
+        create(1, 0.0, 200.0, 1500.0);
+
     }
 
 }
