b471a9e75ee3df31141c37880546296b1e009ef5
broeninkeg <gerben.broenink@tno.nl>
2014-09-01 15:08:43 +0200

    dishwasher simulation added...

---
+++ flexiblepower.simulation.dishwasher/src/org/flexiblepower/miele/dishwasher/driver/DishwasherDriverFactory.java
@@ -0,0 +1,36 @@
+package org.flexiblepower.miele.dishwasher.driver;
+
+import org.flexiblepower.protocol.mielegateway.api.ActionPerformer;
+import org.flexiblepower.protocol.mielegateway.api.MieleResourceDriverFactory;
+import org.flexiblepower.ral.drivers.dishwasher.DishwasherControlParameters;
+import org.flexiblepower.ral.drivers.dishwasher.DishwasherState;
+import org.flexiblepower.time.TimeService;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+
+@Component(provide = MieleResourceDriverFactory.class)
+public class DishwasherDriverFactory extends
+                                    MieleResourceDriverFactory<DishwasherState, DishwasherControlParameters, DishwasherDriver> {
+
+    public DishwasherDriverFactory() {
+        super(DishwasherState.class);
+    }
+
+    private TimeService timeService;
+
+    @Reference
+    public void setTimeService(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @Override
+    public boolean canHandleType(String type) {
+        return "DW_G1000".equals(type) || "G5985          ".equals(type);
+    }
+
+    @Override
+    public DishwasherDriver create(ActionPerformer actionPerformer) {
+        return new DishwasherDriver(actionPerformer, timeService);
+    }
+}
