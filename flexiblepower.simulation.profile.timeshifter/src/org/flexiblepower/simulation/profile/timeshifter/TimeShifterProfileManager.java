e839e71247557e0d98ed98524f49da1519d0a82a
Jan Sipke van der Veen <jan_sipke.vanderveen@tno.nl>
2015-03-23 13:45:15 +0100

    Made TimeShifterProfileManager responsible for a single timeshifter device

--- flexiblepower.simulation.profile.timeshifter/src/org/flexiblepower/simulation/profile/timeshifter/TimeShifterProfileManager.java
+++ flexiblepower.simulation.profile.timeshifter/src/org/flexiblepower/simulation/profile/timeshifter/TimeShifterProfileManager.java
@@ -11,20 +11,31 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NavigableMap;
 import java.util.TreeMap;
+import java.util.UUID;
 import java.util.concurrent.ScheduledFuture;
 
 import javax.measure.Measure;
+import javax.measure.quantity.Power;
 import javax.measure.unit.SI;
 
 import org.flexiblepower.context.FlexiblePowerContext;
 import org.flexiblepower.efi.TimeShifterResourceManager;
+import org.flexiblepower.efi.timeshifter.SequentialProfile;
+import org.flexiblepower.efi.timeshifter.TimeShifterAllocation;
 import org.flexiblepower.efi.timeshifter.TimeShifterRegistration;
+import org.flexiblepower.efi.timeshifter.TimeShifterUpdate;
 import org.flexiblepower.messaging.Connection;
 import org.flexiblepower.messaging.Endpoint;
 import org.flexiblepower.messaging.MessageHandler;
+import org.flexiblepower.ral.messages.AllocationStatus;
+import org.flexiblepower.ral.messages.AllocationStatusUpdate;
+import org.flexiblepower.ral.values.CommodityForecast;
+import org.flexiblepower.ral.values.CommodityForecast.Builder;
 import org.flexiblepower.ral.values.CommoditySet;
+import org.flexiblepower.ral.values.UncertainMeasure;
 import org.flexiblepower.simulation.profile.timeshifter.TimeShifterProfileManager.Config;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -45,10 +56,16 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
         @Meta.AD(deflt = "timeshifterprofilemanager", description = "Resource identifier")
         String resourceId();
 
-        @Meta.AD(deflt = "main.csv",
-                 description = "Main CSV file with references to valid from times, end before times, and power data profile files")
-                String
-                filename();
+        @Meta.AD(deflt = "washingmachine1.csv", description = "CSV file with power data profile")
+        String filename();
+
+        @Meta.AD(deflt = "0",
+                 description = "This profile has validFrom value of activation time plus this value in minutes")
+        int validFrom();
+
+        @Meta.AD(deflt = "600",
+                 description = "This profile has endBefore value of activation time plus this value in minutes")
+        int endBefore();
 
         @Meta.AD(deflt = "5", description = "Delay between updates will be send out in seconds")
         int updateDelay();
@@ -61,7 +78,12 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
     private Connection connection;
     private FlexiblePowerContext context;
     private ScheduledFuture<?> scheduledFuture;
-    private List<TimeShifterProfile> timeShifterProfiles;
+    private float[] powerAtMinutesSinceStart;
+    private boolean requestSent = false;
+    private UUID allocationId = null;
+    private boolean finishSent = false;
+    private long bundleActivationTime;
+    private long allocationReceivedTime;
 
     @Activate
     public void activate(BundleContext bundleContext, Map<String, Object> properties) throws IOException {
@@ -69,7 +91,8 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
             this.bundleContext = bundleContext;
             config = Configurable.createConfigurable(Config.class, properties);
 
-            loadMainData(getInputStreamForFilename(config.filename()));
+            bundleActivationTime = context.currentTimeMillis();
+            loadData(getInputStreamForFilename(config.filename()));
 
             scheduledFuture = context.scheduleAtFixedRate(this,
                                                           Measure.valueOf(0, SI.SECOND),
@@ -115,8 +138,24 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
 
     @Override
     public void handleMessage(Object message) {
-        // TODO Check if the message is of class TimeShifterAllocation
-        // TODO Send AllocationStatusUpdate.ACCEPTED + STARTED
+        if (message != null && message instanceof TimeShifterAllocation) {
+            TimeShifterAllocation allocation = (TimeShifterAllocation) message;
+            allocationId = allocation.getResourceMessageId();
+
+            AllocationStatusUpdate allocationStatusUpdate = new AllocationStatusUpdate(config.resourceId(),
+                                                                                       context.currentTime(),
+                                                                                       allocationId,
+                                                                                       AllocationStatus.ACCEPTED,
+                                                                                       "");
+            connection.sendMessage(allocationStatusUpdate);
+
+            allocationStatusUpdate = new AllocationStatusUpdate(config.resourceId(),
+                                                                context.currentTime(),
+                                                                allocationId,
+                                                                AllocationStatus.STARTED,
+                                                                "");
+            connection.sendMessage(allocationStatusUpdate);
+        }
     }
 
     @Override
@@ -133,33 +172,17 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
         return (1 - fraction) * value1 + fraction * value2;
     }
 
-    private void loadMainData(InputStream is) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
-
-        timeShifterProfiles = new ArrayList<TimeShifterProfile>();
-        String line = null;
-        while ((line = bufferedReader.readLine()) != null) {
-            if (!line.startsWith("#")) { // Line does not contain comment
-                String[] split = line.split(",");
-                if (split.length == 3) {
-                    int validFrom = Integer.parseInt(split[0].trim());
-                    int endBefore = Integer.parseInt(split[1].trim());
-                    String powerProfileFile = split[2].trim();
-                    NavigableMap<Float, Float> powerAtMinutesSinceStart = loadPowerData(getInputStreamForFilename(powerProfileFile));
-                    timeShifterProfiles.add(new TimeShifterProfile(validFrom, endBefore, powerAtMinutesSinceStart));
-                }
-            }
-        }
-        bufferedReader.close();
-    }
-
-    private NavigableMap<Float, Float> loadPowerData(InputStream is) throws IOException {
+    private void loadData(InputStream is) throws IOException {
         // Data gathered from: http://blog.oliverparson.co.uk/2011/01/appliance-survey-tumble-dryer-washing.html
         // Transformed to CSV using: http://arohatgi.info/WebPlotDigitizer/app
 
+        // Read profile data from file. <minute since start, power in watt>
+        // 0.35134207716943777, 6.137829084209898
+        // 0.7084667785064305, 8.747102870387153
+        // 1.065591479843425, 8.747102870387153
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
 
-        NavigableMap<Float, Float> powerAtMinutesSinceStart = new TreeMap<Float, Float>();
+        NavigableMap<Float, Float> powerAtMinutesSinceStartFromPlot = new TreeMap<Float, Float>();
         String line = null;
         while ((line = bufferedReader.readLine()) != null) {
             if (!line.startsWith("#")) { // Line does not contain comment
@@ -167,13 +190,28 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
                 if (split.length == 2) {
                     float minute = Float.parseFloat(split[0].trim());
                     float powerValue = Float.parseFloat(split[1].trim());
-                    powerAtMinutesSinceStart.put(minute, powerValue);
+                    powerAtMinutesSinceStartFromPlot.put(minute, powerValue);
                 }
             }
         }
         bufferedReader.close();
 
-        return powerAtMinutesSinceStart;
+        // Interpolate profile data to an array with index of minute
+        // [0] =
+        // [1] =
+        // [2] =
+        int lastMinute = (int) (float) powerAtMinutesSinceStartFromPlot.lastKey();
+        powerAtMinutesSinceStart = new float[lastMinute];
+        for (int i = 0; i < lastMinute; i++) {
+            Entry<Float, Float> floorEntry = powerAtMinutesSinceStartFromPlot.floorEntry((float) i);
+            Entry<Float, Float> ceilingEntry = powerAtMinutesSinceStartFromPlot.ceilingEntry((float) i);
+            if (floorEntry != null && ceilingEntry != null) {
+                float powerValue1 = floorEntry.getValue();
+                float powerValue2 = ceilingEntry.getValue();
+                double fraction = (i - floorEntry.getKey()) / (ceilingEntry.getKey() - floorEntry.getKey());
+                powerAtMinutesSinceStart[i] = (float) interpolate(powerValue1, powerValue2, fraction);
+            }
+        }
     }
 
     @Override
@@ -181,8 +219,43 @@ public class TimeShifterProfileManager implements TimeShifterResourceManager, Ru
         try {
             if (connection != null) {
                 Date currentTime = context.currentTime();
-                // TODO Send TimeShifterUpdate if a simulated appliance wants to be started (current time >= valid from)
-                // TODO Send AllocationStatusUpdate.FINISHED if simulated appliance has finished
+
+                if (!requestSent && bundleActivationTime + config.validFrom() <= currentTime.getTime()) {
+                    Builder builder = CommodityForecast.create();
+                    builder.duration(Measure.valueOf(60, SI.SECOND));
+                    for (float element : powerAtMinutesSinceStart) {
+                        builder.electricity(new UncertainMeasure<Power>(element, SI.WATT))
+                               .next();
+                    }
+
+                    CommodityForecast commodityForecast = builder.build();
+                    SequentialProfile sequentialProfile = new SequentialProfile(0,
+                                                                                Measure.zero(SI.SECOND),
+                                                                                commodityForecast);
+                    List<SequentialProfile> sequentialProfiles = new ArrayList<SequentialProfile>();
+                    sequentialProfiles.add(sequentialProfile);
+
+                    TimeShifterUpdate timeShifterUpdate = new TimeShifterUpdate(config.resourceId(),
+                                                                                currentTime,
+                                                                                new Date(bundleActivationTime + 60000
+                                                                                         * config.validFrom()),
+                                                                                new Date(bundleActivationTime + 60000
+                                                                                         * config.endBefore()),
+                                                                                sequentialProfiles);
+                    connection.sendMessage(timeShifterUpdate);
+                    requestSent = true;
+                }
+
+                if (allocationId != null && !finishSent
+                    && allocationReceivedTime + powerAtMinutesSinceStart.length * 60000 <= currentTime.getTime()) {
+                    AllocationStatusUpdate allocationStatusUpdate = new AllocationStatusUpdate(config.resourceId(),
+                                                                                               currentTime,
+                                                                                               allocationId,
+                                                                                               AllocationStatus.FINISHED,
+                                                                                               "");
+                    connection.sendMessage(allocationStatusUpdate);
+                    finishSent = true;
+                }
             }
         } catch (Exception e) {
             logger.error("Error while running timeshifter profile manager", e);
