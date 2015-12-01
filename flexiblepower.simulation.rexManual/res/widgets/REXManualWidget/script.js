c80e664bb108401529cc39c189966c6943fc874e
Tomaž Vinko <tomaz@zenodys.com>
2015-12-01 23:44:01 +0100

    RexManualWidget design

--- flexiblepower.simulation.rexManual/res/widgets/REXManualWidget/script.js
+++ flexiblepower.simulation.rexManual/res/widgets/REXManualWidget/script.js
@@ -1,12 +1,22 @@
-$(window).load(function() {
-	w = new widget("update", 1000, function(data) {
-		$("#loading").detach();
-		$("p").show();
-		$(".error").hide();
-	});
-	
-	$("#set_demand").click(function () 
-	{
-		w.call("setDemand", $("#txtDemand").val(), w.callback);
+$(window).load(function () {
+    
+    w = new widget("update", 1000, function (data) {
+        $("#loading").detach();
+        $("p").show();
+        $(".error").hide();
     });
+
+    $("#set_demand").click(function () {
+        w.call("setDemand", $("#txtDemand").val(), w.callback);
+    });
+
+    $("#on").click(function () {
+        w.call("setDemand", "-100000", w.callback);
+    });
+
+    $("#off").click(function () {
+        w.call("setDemand", "100000", w.callback);
+    });
+
+    w.call("setDemand", "0", w.callback);
 });
