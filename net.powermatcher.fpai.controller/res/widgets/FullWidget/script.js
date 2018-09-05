a9f8d5b96b49d2dda5e2bc62ece4d8c6c0396e70
JP Wijbenga <jan_pieter.wijbenga@tno.nl>
2018-09-05 18:31:26 +0900

    Fixed bug in Full widget for when Powermatcher component name includes a period character.

--- net.powermatcher.fpai.controller/res/widgets/FullWidget/script.js
+++ net.powermatcher.fpai.controller/res/widgets/FullWidget/script.js
@@ -2,12 +2,13 @@ $(window).load(function() {
 	w = new widget("update", 2000, function(data) {
 		$(".error").hide();
 		for(var agentId in data) {
-			var div = $("#bid-"+agentId);
+			var agentName = agentId.replace(/\./g, '_');
+			var div = $("#bid-"+agentName);
 			if(div.size() == 0) {
-				$("#bids").append('<div id="bid-'+agentId+'"><p>'+agentId+'</p><div style="width: 400px; height: 125px;"></div></div>');
-				var div = $("#bid-"+agentId);
+				$("#bids").append('<div id="bid-'+agentName+'"><p>'+agentName+'</p><div style="width: 400px; height: 125px;"></div></div>');
+				var div = $("#bid-"+agentName);
 			}
-			$.plot("#bid-"+agentId+" div", [ data[agentId].coordinates, 
+			$.plot("#bid-"+agentName+" div", [ data[agentId].coordinates, 
 			                                 [[data[agentId].price,-data[agentId].maxDemand],
 			                                  [data[agentId].price,data[agentId].maxDemand]
 			                                 ]]);
